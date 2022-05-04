ae8aef56a95337c42cdc25e09c51279ae089392a
Y4HYA4 <yahia.aboulhaj@istic.ucar.tn>
2022-05-04 05:33:10 +0200

    testing, yml config back & front

--- backend/test/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req, data) {
-    context.bindings.outputDocument = data[0];
-    context.bindings.outputDocument.count += 1;
-    context.res = {
-        body: context.bindings.outputDocument.count
-    };
-}
